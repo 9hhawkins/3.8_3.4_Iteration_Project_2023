a27854b052104b2695ae2664952b05fa7ae83479
9hhawkins <137360423+9hhawkins@users.noreply.github.com>
2023-08-03 12:42:32 +1200

    Create slideshow.js

---
+++ js/slideshow.js
@@ -0,0 +1,27 @@
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"];
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  console.log(x);
+  if (no < 0 || no >= slideIndex.length) {
+    console.error("Invalid index for slideId: " + no);
+    return;
+  }
+
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
