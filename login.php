adecfd2a876034c423df8750174193946c98de61
9hhawkins <137360423+9hhawkins@users.noreply.github.com>
2023-09-21 22:59:30 +1200

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	ob_start();
 	session_start();
 	$error = NULL;
-	$admin = NULL;
+	$_SESSION['admin'] = NULL;
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		require "91902_Assessment_HHawkins_mysqli.php";
 		
@@ -16,10 +16,10 @@
 		
 		$count = mysqli_num_rows($result);
 		
-		if($count == 1 and $myusername == "Graeme" and $mypassword == 123){
+		if($count == 1 and $myusername == "Graeme"){
 			$_SESSION['login_user'] = $myusername;
 			$_SESSION['admin'] = "True";
-			header("location: home.php");
+			header("location: admin_home.php"); //change to admin page when made
 		} 
 		elseif($count == 1){
 			$_SESSION['login_user'] = $myusername;
@@ -50,12 +50,14 @@
                                 <center><div class="content">
 									<!-- Holds the main page content -->
 										<h3><form method="post" id="login">
-											<input type="text" name="username" placeholder="Enter user name"/><br/>
-											<input type="password" name="password" placeholder="Enter user password"/><br/>
-											<input type="submit" value="Login"/><br />
+											<label><input type="text" name="username" placeholder="Enter user name" style="background-image: url(images/user.png);"/></label>
+											<label><input type="password" name="password" placeholder="Enter user password" style="background-image: url(images/padlock.png);"/></label>
+											<input type="submit" value="Login"/>
 										</form></h3>
 										<h3><?php echo $error; ?></h3>
-								</div></center>
+									<a href="sign_up.php"><p>Don't have an account click here</p></a>
+								</div>
+							</center>
                         </div>
 					<footer>
             		<p>&copy Copyright Harrison Hawkins 2023</p>
