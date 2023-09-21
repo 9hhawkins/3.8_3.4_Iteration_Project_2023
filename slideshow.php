e8344ce01fe275b2881981ae5e82bd27b5a29025
9hhawkins <137360423+9hhawkins@users.noreply.github.com>
2023-09-21 23:03:23 +1200

    Create slideshow.php

---
+++ slideshow.php
@@ -0,0 +1,92 @@
+<!-- Slideshow container -->
+		<div class="text">Recommended</div>
+		<div class="slideshow-container">
+		  <!-- Full-width images with number and caption text -->
+		  	<div class="mySlides1">
+				<a href="playlist.php?img=images/Rock.png&query=Genre_Query&genre=Rock"><img src="images/Rock.png"></a>
+				<a href="playlist.php?img=images/Country.png&query=Genre_Query&genre=Country"><img src="images/Country.png"></a>
+				<a href="playlist.php?img=images/Folk.png&query=Genre_Query&genre=Folk"><img src="images/Folk.png"></a>
+				<a href="playlist.php?img=images/New_Wave.png&query=Genre_Query&genre=New Wave"><img src="images/New_Wave.png"></a>
+		  	</div>
+
+		  	<div class="mySlides1">
+				<a href="playlist.php?img=images/Jazz.png&query=Genre_Query&genre=Jazz"><img src="images/Jazz.png"></a>
+				<a href="playlist.php?img=images/Indie.png&query=Genre_Query&genre=Indie"><img src="images/Indie.png"></a>
+				<a href="playlist.php?img=images/Pop.png&query=Genre_Query&genre=Pop"><img src="images/Pop.png"></a>
+				<a href="playlist.php?img=images/Blues.png&query=Genre_Query&genre=Blues"><img src="images/Blues.png"></a>
+		  	</div>
+
+		  	<div class="mySlides1">
+				<a href="playlist.php?img=images/Celtic.png&query=Genre_Query&genre=Celtic"><img src="images/Celtic.png"></a>
+				<a href="playlist.php?img=images/Indie_Rock.png&query=Genre_Query&genre=Indie Rock"><img src="images/Indie_Rock.png"></a>
+				<a href="playlist.php?img=images/Alternative.png&query=Genre_Query&genre=Alternative"><img src="images/Alternative.png"></a>
+				<a href="playlist.php?img=images/Reggae.png&query=Genre_Query&genre=Reggae"><img src="images/Reggae.png"></a>
+		 	 </div>
+
+		  <!-- Next and previous buttons -->
+		  	<a class="prev" onclick="plusSlides(-1, 0)">&#10094;</a>
+		  	<a class="next" onclick="plusSlides(1, 0)">&#10095;</a>
+			</div>
+			<br>
+		
+				<!-- Slideshow container -->
+		<div class="text">Popular</div>
+		<div class="slideshow-container">
+		  <!-- Full-width images with number and caption text -->
+		  	<div class="mySlides2">
+				<a href="playlist.php?img=images/Jazz.png&query=Genre_Query&genre=Jazz"><img src="images/Jazz.png"></a>
+				<a href="playlist.php?img=images/Country.png&query=Genre_Query&genre=Country"><img src="images/Country.png"></a>
+				<a href="playlist.php?img=images/Folk.png&query=Genre_Query&genre=Folk"><img src="images/Folk.png"></a>
+				<a href="playlist.php?img=images/New_Wave.png&query=Genre_Query&genre=New Wave"><img src="images/New_Wave.png"></a>
+		  	</div>
+
+		  	<div class="mySlides2">
+				<a href="playlist.php?img=images/Rock.png&query=Genre_Query&genre=Rock"><img src="images/Rock.png"></a>
+				<a href="playlist.php?img=images/Indie.png&query=Genre_Query&genre=Indie"><img src="images/Indie.png"></a>
+				<a href="playlist.php?img=images/Pop.png&query=Genre_Query&genre=Pop"><img src="images/Pop.png"></a>
+				<a href="playlist.php?img=images/Blues.png&query=Genre_Query&genre=Blues"><img src="images/Blues.png"></a>
+		  	</div>
+
+		  	<div class="mySlides2">
+				<a href="playlist.php?img=images/Indie_Rock.png&query=Genre_Query&genre=Indie Rock"><img src="images/Indie_Rock.png"></a>
+				<a href="playlist.php?img=images/Alternative.png&query=Genre_Query&genre=Alternative"><img src="images/Alternative.png"></a>
+				<a href="playlist.php?img=images/Reggae.png&query=Genre_Query&genre=Reggae"><img src="images/Reggae.png"></a>
+				<a href="playlist.php?img=images/Celtic.png&query=Genre_Query&genre=Celtic"><img src="images/Celtic.png"></a>
+		 	 </div>
+
+		  <!-- Next and previous buttons -->
+		  	<a class="prev" onclick="plusSlides(-1, 1)">&#10094;</a>
+		  	<a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
+			</div>
+			<br>
+		<!-- Slideshow container -->
+		<div class="text">Discover picks for you</div>
+		<div class="slideshow-container">
+		  <!-- Full-width images with number and caption text -->
+		  	<div class="mySlides3">
+				<a href="playlist.php?img=images/Alternative.png&query=Genre_Query&genre=Alternative"><img src="images/Alternative.png"></a>
+				<a href="playlist.php?img=images/Reggae.png&query=Genre_Query&genre=Reggae"><img src="images/Reggae.png"></a>
+				<a href="playlist.php?img=images/Celtic.png&query=Genre_Query&genre=Celtic"><img src="images/Celtic.png"></a>
+				<a href="playlist.php?img=images/Indie_Rock.png&query=Genre_Query&genre=Indie Rock"><img src="images/Indie_Rock.png"></a>
+		  	</div>
+
+		  	<div class="mySlides3">
+				<a href="playlist.php?img=images/Folk.png&query=Genre_Query&genre=Folk"><img src="images/Folk.png"></a>
+				<a href="playlist.php?img=images/New_Wave.png&query=Genre_Query&genre=New Wave"><img src="images/New_Wave.png"></a>
+				<a href="playlist.php?img=images/Rock.png&query=Genre_Query&genre=Rock"><img src="images/Rock.png"></a>
+				<a href="playlist.php?img=images/Country.png&query=Genre_Query&genre=Country"><img src="images/Country.png"></a>
+		  	</div>
+
+		  	<div class="mySlides3">
+				<a href="playlist.php?img=images/Pop.png&query=Genre_Query&genre=Pop"><img src="images/Pop.png"></a>
+				<a href="playlist.php?img=images/Blues.png&query=Genre_Query&genre=Blues"><img src="images/Blues.png"></a>
+				<a href="playlist.php?img=images/Jazz.png&query=Genre_Query&genre=Jazz"><img src="images/Jazz.png"></a>
+				<a href="playlist.php?img=images/Indie.png&query=Genre_Query&genre=Indie"><img src="images/Indie.png"></a>
+		 	 </div>
+
+		  <!-- Next and previous buttons -->
+		  	<a class="prev" onclick="plusSlides(-1, 2)">&#10094;</a>
+		  	<a class="next" onclick="plusSlides(1, 2)">&#10095;</a>
+			</div>
+			<br>
+		<script src="js/slideshow.js"></script>
